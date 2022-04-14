0cceb8ba06e91bed5b9e43506fb511566d51687a
hellenribas <tchwtrabalho@gmail.com>
2022-04-14 15:19:12 -0300

    refatorando para publicar

--- script.js
+++ script.js
@@ -1,62 +1,63 @@
-function pegaId (id) {
-    return document.getElementById(id);
-};
-function criarElem (tag) {
-    return document.createElement(tag)
-};
-function evento (pai, type, callback) {
-pai.addEventListener(type, callback);
-};
-function texto() {
-   const texto = pegaId('meme-text');
-   const input = pegaId('text-input');
-   texto.innerText = input.value;
+const meme = 'meme-image';
 
-};
+function pegaId(id) {
+  return document.getElementById(id);
+}
+function criarElem(tag) {
+  return document.createElement(tag);
+}
+function evento(pai, type, callback) {
+  pai.addEventListener(type, callback);
+}
+function texto() {
+  const textos = pegaId('meme-text');
+  const input = pegaId('text-input');
+  textos.innerText = input.value;
+}
 // usei como referência o video https://www.youtube.com/watch?v=lzK8vM_wdoY&t=236s//
 let upload = '';
-function images () {
-   const images = pegaId('meme-insert');
-   const leitor = new FileReader();
-   evento(leitor, 'load', function() {
-       upload = leitor.result;
-       pegaId('meme-image').style.backgroundImage = `url(${upload})`;
-
-   });
-   leitor.readAsDataURL(images.files[0]);
-       
+function images() {
+  const imagesAdd = pegaId('meme-insert');
+  const leitor = new FileReader();
+  evento(leitor, 'load', () => {
+    upload = leitor.result;
+    pegaId(meme).style.backgroundImage = '';
+    pegaId(meme).style.backgroundImage = `url(${upload})`;
+  });
+  leitor.readAsDataURL(imagesAdd.files[0]);
 }
-function botaoBorda (cor, id, nomeBotao) {
-    const pai = pegaId('botao');
-    let botao = criarElem('button');
-    botao.style.backgroundColor = cor;
-    botao.id = id;
-    let text = document.createTextNode(nomeBotao);
-    botao.appendChild(text);
-    pai.appendChild(botao);
-    return botao;
+function botaoBorda(cor, id, nomeBotao) {
+  const pai = pegaId('botao');
+  const botao = criarElem('button');
+  botao.style.backgroundColor = cor;
+  botao.id = id;
+  const text = document.createTextNode(nomeBotao);
+  botao.appendChild(text);
+  pai.appendChild(botao);
+  return botao;
 }
-function borda (elem) {
-    const pai = pegaId('meme-image-container');
-    pai.style.border = elem;
+function borda(elem) {
+  const pai = pegaId('meme-image-container');
+  pai.style.border = elem;
 }
-function imgExtra (elem) {
-    if (elem.id !== 'memes') {
-  pegaId('meme-image').src = elem.src;  
-}
+function imgExtra(elem) {
+  if (elem.id !== 'memes') {
+    pegaId(meme).style.backgroundImage = '';
+    pegaId(meme).src = elem.src;
+  }
 }
 
 evento(pegaId('text-input'), 'input', texto);
 evento(pegaId('meme-insert'), 'change', images);
-evento(botaoBorda('red', 'fire', 'Vermelho'), 'click',function () {
+evento(botaoBorda('red', 'fire', 'Vermelho'), 'click', () => {
   borda('3px dashed red');
-})
-evento(botaoBorda('blue', 'water', 'Azul'), 'click', function (){
+});
+evento(botaoBorda('blue', 'water', 'Azul'), 'click', () => {
   borda('5px double blue');
 });
-evento(botaoBorda('green', 'earth', 'Verde'), 'click', function () {
+evento(botaoBorda('green', 'earth', 'Verde'), 'click', () => {
   borda('6px groove green');
 });
-evento(pegaId('memes'), 'click',function (event) {
-    imgExtra(event.target);
+evento(pegaId('memes'), 'click', (event) => {
+  imgExtra(event.target);
 });
